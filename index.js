ce899e2fc48a89ce9ae79f93cc4b471f5e9b07b4
alvin2ye <alvin.ye.cn@gmail.com>
2014-09-01 14:44:27 +0800

    add test

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var moment = require('moment');
+
 var now = moment().format('MMMM Do YYYY, h:mm:ss a');
 
 module.exports = now + " hello world";
--- tests/hello-world-spmjs3-spec.js
+++ tests/hello-world-spmjs3-spec.js
@@ -4,7 +4,7 @@ var helloWorldSpmjs3 = require('../index');
 describe('hello-world-spmjs3', function() {
 
   it('normal usage', function() {
-
+    expect(helloWorldSpmjs3).to.be.a('string');  // add this
   });
 
 });
